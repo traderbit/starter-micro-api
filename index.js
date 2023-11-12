581041f177c957957ad946a78629b80ed88f6f47
traderbit <150610309+traderbit@users.noreply.github.com>
2023-11-12 23:03:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/download', (req, res) => {
   res.send('Hello World!')
 })
 
