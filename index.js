05b34a8a5b041d7212cb89fcbf2e28866c87cbe7
traderbit <150610309+traderbit@users.noreply.github.com>
2023-11-14 13:44:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.configure(function(){
-  app.set("view options", {layout: false});
-
-  // make a custom html template
-  app.register('.html', {
-    compile: function(str, options){
-      return function(locals){
-        return str;
-      };
-    }
-  });
-});
-
 app.get('/a', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/', (req, res) => {
-    res.render("index.html");
+    res.sendfile('./index.html');
 });
 
 app.listen(port, () => {
