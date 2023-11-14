a1307839f47bf00e029398d204f9340463faa276
traderbit <150610309+traderbit@users.noreply.github.com>
2023-11-14 13:39:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/a', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/', (req, res) => {
+    fs.readFile(__dirname + '/public/index.html', 'utf8', (err, text) => {
+        res.send(text);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
