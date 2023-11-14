f4c7656122430b1a0f84e9e8534da2952a4b7103
traderbit <150610309+traderbit@users.noreply.github.com>
2023-11-14 14:23:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendfile('./index.html');
 });
 
+app.post('/download', (req, res) => {
+    res.json({'success' : true});
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
