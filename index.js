369c15fe4816ed2d866529cfa3df61afaa692617
traderbit <150610309+traderbit@users.noreply.github.com>
2023-11-14 14:59:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post('/download', async(req, res) => {
       try {
     const apiUrl = 'https://fobrcb-ip-103-38-69-232.tunnelmole.net/fetch/'+url; // Replace with your API endpoint
     const response = await axios.get(apiUrl);
-    res.json(response);
+    res.json(response.data);
   } catch (error) {
     // Handle errors
     res.status(500).json({ error: 'Internal Server Error' });
