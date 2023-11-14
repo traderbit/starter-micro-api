a4a6bb25c742192965df8af0b9ec2e7aef95863e
traderbit <150610309+traderbit@users.noreply.github.com>
2023-11-14 16:31:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const axios = require('axios');
+const mime = require('mime');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
