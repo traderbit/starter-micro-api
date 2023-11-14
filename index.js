487538bd76e835c2c72b82812817756e746e9df6
traderbit <150610309+traderbit@users.noreply.github.com>
2023-11-14 14:44:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/a', (req, res) => {
